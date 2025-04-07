6e011fc15c783ec1300dcd21c3b902db87742657
laurad304 <laura.debowska304@gmail.com>
2025-04-07 09:35:45 +0200

    wip: function get_user_info, prepared

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ users:list[dict] =[
 ]
 
 
-for user in users:
-    print(f'Twój znajomy: {user["name"]} z miejscowości {user["location"]} opublikował {user["post"]} postów')
+def get_user_info (users_data:list[dict])->None:
+    for user in users_data:
+        print(f'Twój znajomy: {user["name"]} z miejscowości {user["location"]} opublikował {user["post"]} postów')
+
+get_user_info(users)
